d0fd17299e9729a1377214678d69bd092738dd7f
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-11 15:28:59 +0430

    create structure of authentication controller

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        # code...
+    }
+
+    public function login(Request $request)
+    {
+        # code...
+    }
+
+    public function logout()
+    {
+        # code...
+    }
+}
