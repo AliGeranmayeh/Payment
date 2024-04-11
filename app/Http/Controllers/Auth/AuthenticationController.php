042d500ae6520a99f1d16060d3b293c81c8f5631
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-11 16:38:11 +0430

    create login form request and use it

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Helpers\DB\UserRepository;
 use App\Helpers\Responses\AuthenticationResponse;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AuthenticationController extends Controller
 {
@@ -17,7 +17,7 @@ class AuthenticationController extends Controller
         return AuthenticationResponse::register($user);
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         # code...
     }
