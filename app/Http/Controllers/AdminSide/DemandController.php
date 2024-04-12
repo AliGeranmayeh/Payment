48c21489005c05ecd85317f2a13967c1e2e8ed3d
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-12 12:17:04 +0430

    create structure of admin side demand controller

---
+++ app/Http/Controllers/AdminSide/DemandController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\AdminSide;
+
+use App\Http\Controllers\Controller;
+use App\Models\Demand;
+
+class DemandController extends Controller
+{
+    public function index()
+    {
+        # code...
+    }
+
+    public function show(Demand $demand)
+    {
+        # code...
+    }
+
+    public function changeStatus(Demand $demand)
+    {
+        # code...
+    }
+}
