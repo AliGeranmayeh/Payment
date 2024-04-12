b9ff34484379999edd0a732ec8faaeccb04478d6
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-12 11:23:33 +0430

    create demand resource

---
+++ app/Http/Resources/DemandResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Demand;
+use App\Enums\DemandStatusEnum;
+
+class DemandResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'description' =>$this->description,
+            'cost' =>$this->cost,
+            'shaba' =>$this->shaba,
+            'file' =>$this->file,
+            'status' =>$this->status ?? DemandStatusEnum::PENDING,
+            'category' => $this->category->name
+        ];
+    }
+}
