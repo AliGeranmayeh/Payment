16e2e826e70e1eea1f96f1596fe153face6d8c8b
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-12 17:30:06 +0430

    create  form request for adding reply on demands

--- app/Http/Controllers/AdminSide/ReplyController.php
+++ app/Http/Controllers/AdminSide/ReplyController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers\AdminSide;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateReplyRequest;
 
 class ReplyController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(CreateReplyRequest $request)
     {
         //
     }
