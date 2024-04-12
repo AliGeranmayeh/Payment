34ebf343a8194d56726bcbed0c96d2e0b135cdb9
AliGeranmayeh <aligeranmayeh9797@gmail.com>
2024-04-12 13:05:47 +0430

    complete showing admin side  demands controller logic

--- app/Http/Controllers/AdminSide/DemandController.php
+++ app/Http/Controllers/AdminSide/DemandController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\AdminSide;
 
 use App\Http\Controllers\Controller;
 use App\Models\Demand;
+use App\Helpers\DB\DemandRepository;
+use App\Helpers\Responses\DemandResponse;
 
 class DemandController extends Controller
 {
     public function index()
     {
-        # code...
+        $demands = DemandRepository::all();
+
+        return DemandResponse::index($demands);
     }
 
     public function show(Demand $demand)
